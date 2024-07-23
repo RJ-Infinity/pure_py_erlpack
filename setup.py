63169e7ca219cb420a065429e4f82fcff738f713
RJ_Infinity <rjinfinity0@gmail.com>
2024-07-23 20:28:43 +0100

    fixed the metadata

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name='erlpack',
+    name='pure_py_erlpack',
     version='1.0.0',
-    author='Jake Heinz',
-    author_email='jh@discordapp.com',
-    url="http://github.com/discord/erlpack",
-    description='A high performance erlang term encoder for Python.',
+    author='RJ_Infinity',
+    author_email='rjinfinity0@gmail.com',
+    url="http://github.com/RJ-Infinity/pure_py_erlpack",
+    description='A erlang term encoder for Python.',
     license='Apache 2.0',
     zip_safe=False,
     package_dir={'': 'py'},
